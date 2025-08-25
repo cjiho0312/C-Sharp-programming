5359f6e0088472247a2e8600f1b66cf198b068dc
cjiho0312 <cjiho0312@naver.com>
2025-08-25 15:42:36 +0900

    2025.8.25 매개 변수 한정자

---
+++ Program/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 박싱
+            // 값 형식의 데이터를 참조 형식으로 변환하는 과정입니다.
+
+            // int value = 100;
+
+            // object generic = value;
+
+            // Console.WriteLine("value : " +  value);
+            // Console.WriteLine("generic : " + generic);
+
+            #endregion
+
+            #region 언박싱
+            // 참조 형식의 데이터를 값 형식으로 변환하는 과정입니다.
+
+            // int box = (int)generic; // 명시적 형변환
+
+            // Console.WriteLine("box : " + box);
+
+            #endregion
+
+            Utility utility = new Utility();
+
+            // utility.Pause();
+
+            int a = 1;
+            int b = 3;
+
+            int c = -5;
+            
+            utility.Swap(ref a, ref b);
+
+            Console.WriteLine($"a : {a}\nb : {b}");
+            Console.WriteLine("Absolute c : " + utility.Absolute(in c));
+        }
+    }
+}
---
+++ Program/Utility.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    // 매개변수 한정자
+
+    // ref : 참조로 전달 (읽기 / 쓰기)
+    // out : 참조로 전달 (쓰기)
+    // in  : 참조로 전달 (읽기)
+    // params : 가변 인자
+
+    internal class Utility
+    {
+        // 클래스 내부에서는 접근 지정자를 함수나 변수 앞에 붙여줘야 합니다.
+
+        public void Swap(ref int x, ref int y)
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+
+        public int Absolute(in int value)
+        {
+            int number = value;
+
+            if (value < 0)
+            {
+                number = -number;
+            }
+
+            return number;
+        }
+
+        public void Pause()
+        {
+            Console.WriteLine("Pause....");
+        }
+    }
+}
